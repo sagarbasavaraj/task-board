8ec8820a5d2b08e8eba114037ddbbb7e658c524a
sagar basavaraj <sagarbasavaraj@sagars-MBP.local>
2018-02-16 00:10:11 +0100

    added header and footer component

--- src/layouts/footer.js
+++ src/layouts/footer.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import { Grid, Glyphicon, Navbar } from 'react-bootstrap/lib';
 
-const Footer = () => <h1>footer</h1>;
+import './footer.css';
+
+const Footer = () => (
+  <Grid>
+    <div className="l-footer">
+      <label className="l-copyright">
+        Taskboard <Glyphicon glyph="copyright-mark" />2018
+      </label>
+    </div>
+  </Grid>
+);
 
 export default Footer;
--- src/layouts/header.js
+++ src/layouts/header.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import { Navbar, Nav, NavItem } from 'react-bootstrap/lib';
+import { Link } from 'react-router-dom';
 
-const Header = () => <h1>header</h1>;
+const Header = () => (
+  <Navbar inverse staticTop>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <Link to="/">Taskboard</Link>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+    </Navbar.Header>
+    <Navbar.Collapse>
+      <Nav pullRight>
+        <NavItem eventKey={1} href="#">
+          Sign in
+        </NavItem>
+        <NavItem eventKey={2} href="#">
+          Sign up
+        </NavItem>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+);
 
 export default Header;
