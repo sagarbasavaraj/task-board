920d2ce04bbbf66dcfdec5ec2195dd57d905d6d2
sagar basavaraj <sagarbasavaraj@sagars-MBP.local>
2018-03-21 23:38:51 +0100

    resolved typo

--- src/common/render-field.js
+++ src/common/render-field.js
@@ -18,7 +18,7 @@ const renderField = ({
   errorMsg,
   meta: { touched, error, warning }
 }) => {
-  const resolveErrorMsg = resolveDisplayText(t, errorMsg);
+  const resolvedErrorMsg = resolveDisplayText(t, errorMsg);
   const floatingLabelText = resolveDisplayText(t, label, text);
   const translatedHelpContent = resolveDisplayText(t, helpContent);
   return (
@@ -26,7 +26,7 @@ const renderField = ({
       <TextField
         {...input}
         type={type}
-        errorText={error || resolveErrorMsg}
+        errorText={error || resolvedErrorMsg}
         floatingLabelText={floatingLabelText}
       />
       {translatedHelpContent ? (
